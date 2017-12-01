bf19c0072be467b1b636c5725a72d60f28850d1b
Daniel Kowalewski <daniel@dankow.net>
2017-12-01 16:57:56 -0700

    Solution to day 1, problem 1.

--- day01/1/reverse_captcha.py
+++ day01/1/reverse_captcha.py
@@ -8,6 +8,22 @@ import os
 import sys
 import argparse
 
+def sum_seq(infile):
+    sum = 0
+    sequence = []
+    
+    map(sequence.extend, infile.readline())
+#     print(sequence)
+#     print("length = %s" % len(sequence))
+    for i in range(0,len(sequence)-1):
+        if sequence[i] == "\n":
+            break
+        elif sequence[i] == sequence[i+1]:
+            sum += int(sequence[i])
+        elif sequence[i+1] == "\n" and sequence[i] == sequence[0]:
+            sum += int(sequence[i])
+#         print(i, sequence[i], sum)
+    return sum
 
 def main(arguments):
 
@@ -15,12 +31,12 @@ def main(arguments):
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('infile', help="Input file", type=argparse.FileType('r'))
-    parser.add_argument('-o', '--outfile', help="Output file",
-                        default=sys.stdout, type=argparse.FileType('w'))
 
     args = parser.parse_args(arguments)
 
-    print(args)
+    print(sum_seq(args.infile))
+    args.infile.close()
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
